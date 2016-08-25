219c5963a30c85d045db9d8a0f3c877ee910d73e
Jeanette Adelson <jeni.jeanette@gmail.com>
2016-08-25 16:05:04 -0500

    button working

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
+  postJoke()
 
+});
+
+function postJoke() {
   $('#get-joke').on('click', function (e) {
     e.preventDefault();
 
     $.ajax({
       url: 'http://api.icndb.com/jokes/random/',
       method: 'GET'
-    }).done(function (data) {
+    }).done(function(data) {
       console.log('working');
     })
 
 
   });
-
-});
+}
